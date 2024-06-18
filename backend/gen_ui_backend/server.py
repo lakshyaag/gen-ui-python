edf5dfce4c131a846b85e6f2c34bfc7b4e51de3e
Lakshya Agarwal <lakshyajannu@gmail.com>
2024-06-18 17:55:44 -0400

    feat: add debug mode

--- backend/gen_ui_backend/server.py
+++ backend/gen_ui_backend/server.py
@@ -1,3 +1,4 @@
+import asyncio
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -10,32 +11,39 @@ from gen_ui_backend.types import ChatInputType
 # Load environment variables from .env file
 load_dotenv()
 
+app = FastAPI(
+    title="Gen UI Backend",
+    version="1.0",
+    description="A simple api server using Langchain's Runnable interfaces",
+)
+
+# Configure CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+graph = create_graph()
+
+
+runnable = graph.with_types(input_type=ChatInputType, output_type=dict)
+
+add_routes(app, runnable, path="/chat", playground_type="chat")
+
 
 def start() -> None:
-    app = FastAPI(
-        title="Gen UI Backend",
-        version="1.0",
-        description="A simple api server using Langchain's Runnable interfaces",
-    )
-
-    # Configure CORS
-    origins = [
-        "http://localhost",
-        "http://localhost:3000",
-    ]
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    graph = create_graph()
-
-    runnable = graph.with_types(input_type=ChatInputType, output_type=dict)
-
-    add_routes(app, runnable, path="/chat", playground_type="chat")
     print("Starting server...")
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+def debug() -> None:
+    print("Starting server in debug mode...")
+    uvicorn.run("gen_ui_backend.server:app", host="0.0.0.0", port=8000, reload=True)
